6479a06a38d4b64be27cf45e4e43590f2636f4d4
VRAJ2202 <101553398+VRAJ2202@users.noreply.github.com>
2022-12-02 12:33:29 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+import 'screens/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff67864A),
+      ),
+      home: HomePage(),
+    );
+  }
+}
